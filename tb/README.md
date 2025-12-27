1720c88d89d7aa733ece96f98555fa5c005ad614
Caio Alonso da Costa <caioalonso@gmail.com>
2025-12-27 15:19:17 +0100

    New batch of files.

---
+++ tb/README.md
@@ -0,0 +1,42 @@
+# Sample testbench
+
+This is a sample testbench for a digital project. It uses [cocotb](https://docs.cocotb.org/en/stable/) to drive the DUT and check the outputs.
+
+## How to run
+
+To run the RTL simulation:
+
+```sh
+make -B
+```
+
+TBD TODO TO be updated for gatelevel simulation
+To run gatelevel simulation, first harden your project and copy `../runs/wokwi/results/final/verilog/gl/{your_module_name}.v` to `gate_level_netlist.v`.
+
+Then run:
+
+```sh
+make -B GATES=yes
+```
+
+If you wish to save the waveform in VCD format instead of FST format, edit tb.v to use `$dumpfile("tb.vcd");` and then run:
+
+```sh
+make -B FST=
+```
+
+This will generate `tb.vcd` instead of `tb.fst`.
+
+## How to view the waveform file
+
+Using GTKWave
+
+```sh
+gtkwave tb.fst tb.gtkw
+```
+
+Using Surfer
+
+```sh
+surfer tb.fst
+```
---
+++ tb/src/tb.v
@@ -0,0 +1,45 @@
+`default_nettype none
+`timescale 1ns / 1ps
+
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
+module tb ();
+
+  // Wire up the inputs and outputs:
+  reg        rstb;
+  reg        ready;
+  reg  [7:0] data_in;
+  wire [7:0] data_out;
+  wire       r_w;
+  wire       data_vld;
+  wire       start;
+  wire       stop;
+  reg        scl_in;
+  wire       scl_oe;
+  reg        sda_in;
+  wire       sda_oeb;
+
+  i2c_slave i2c_slave_i (
+      .rstb           (rstb),
+      .ready          (ready),
+      .data_in        (data_in),
+      .data_out       (data_out),
+      .r_w            (r_w),
+      .data_vld       (data_vld),
+      .start          (start),
+      .stop           (stop),
+      .scl_in         (scl_in),
+      .scl_oe         (sdl_oe),
+      .sda_in         (sda_in),
+      .sda_oe         (sda_oe)
+  );
+
+  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("tb.fst");
+    $dumpvars(0, tb);
+    #1;
+  end
+
+endmodule
