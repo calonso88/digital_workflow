2a46d886b3b36ce64608ab575c254fecc176eaa5
Caio Alonso da Costa <caioalonso@gmail.com>
2025-12-27 15:24:34 +0100

    Add Makefile.

--- tb/src/tb.v
+++ tb/src/tb.v
@@ -21,18 +21,18 @@ module tb ();
   wire       sda_oeb;
 
   i2c_slave i2c_slave_i (
-      .rstb           (rstb),
-      .ready          (ready),
-      .data_in        (data_in),
-      .data_out       (data_out),
-      .r_w            (r_w),
-      .data_vld       (data_vld),
-      .start          (start),
-      .stop           (stop),
-      .scl_in         (scl_in),
-      .scl_oe         (sdl_oe),
-      .sda_in         (sda_in),
-      .sda_oe         (sda_oe)
+    .rstb      (rstb),
+    .ready     (ready),
+    .data_in   (data_in),
+    .data_out  (data_out),
+    .r_w       (r_w),
+    .data_vld  (data_vld),
+    .start     (start),
+    .stop      (stop),
+    .scl_in    (scl_in),
+    .scl_oe    (sdl_oe),
+    .sda_in    (sda_in),
+    .sda_oe    (sda_oe)
   );
 
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
